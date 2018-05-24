420c987f781e2b6b774bde5ae083a68cf396ea1d
NickDBurton <nickdburton@hotmail.com>
2018-05-24 20:38:35 +0100

    assignment 25 album: completed

--- scripts/album.js
+++ scripts/album.js
@@ -6,10 +6,10 @@ var albumPicasso = {
   albumArtUrl: 'assets/images/album_covers/01.png',
   songs: [
     { title: 'Blue', duration: '4:26' },
-    { title: 'Green', duration: '3:14'},
-    { title: 'Red', duration: '5:01'},
-    { title: 'Pink', duration: '3:21'},
-    { title: 'Magenta', duration: '2:15'},
+    { title: 'Green', duration: '3:14' },
+    { title: 'Red', duration: '5:01' },
+    { title: 'Pink', duration: '3:21' },
+    { title: 'Magenta', duration: '2:15' }
   ]
 };
 
@@ -21,10 +21,25 @@ var albumMarconi = {
   albumArtUrl: 'assets/images/album_covers/20.png',
   songs: [
     { title: 'Hello, Operator', duration: '1:01' },
-    { title: 'Ring, ring, ring', duration: '5:01'},
-    { title: 'Fits in your pocket', duration: '3:21'},
-    { title: 'Can you hear me now?', duration: '3:14'},
-    { title: 'Wrong phone number', duration: '2:15'},
+    { title: 'Ring, ring, ring', duration: '5:01' },
+    { title: 'Fits in your pocket', duration: '3:21' },
+    { title: 'Can you hear me now?', duration: '3:14' },
+    { title: 'Wrong phone number', duration: '2:15' }
+  ]
+};
+
+var albumGhost = {
+  title: 'The Haunting',
+  artist: 'Dee Dopler',
+  label: 'Sony',
+  year: '1895',
+  albumArtUrl: 'assets/images/album_covers/09.png',
+  songs: [
+    { title: 'Mountain Spirit', duration: '7:26' },
+    { title: 'Run, run, run', duration: '3:08' },
+    { title: 'Moonlight song', duration: '5:45' },
+    { title: 'Steep the way?', duration: '6:11' },
+    { title: 'What? What?', duration: '1:25' }
   ]
 };
 
@@ -39,6 +54,13 @@ var createSongRow = function(songNumber, songName, songLength) {
 
     return template;
 };
+
+// Select elements
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
 var setCurrentAlbum = function(album) {
   // #1
@@ -65,4 +87,15 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function() {
   setCurrentAlbum(albumPicasso);
+
+  var albums = [albumPicasso, albumMarconi, albumGhost];
+  var index = 1;
+
+  albumImage.addEventListener("click", function(event) {
+    setCurrentAlbum(album[index]);
+    index++;
+    if (index == albums.length) {
+      index = 0;
+    }
+  });
 };
