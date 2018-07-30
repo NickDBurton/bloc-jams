fc28664e02855b970fdbfbf92ca6518ff3afa97e
NickDBurton <nickdburton@hotmail.com>
2018-07-30 22:39:54 +0100

    Display album title correctly

--- scripts/album.js
+++ scripts/album.js
@@ -174,7 +174,7 @@ var previousSong = function() {
 var updatePlayerBarSong = function() {
   $('.currently-playing .song-name').text(currentSongFromAlbum.title);
   $('.currently-playing .artist-name').text(currentAlbum.artist);
-  $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum);
+  $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.title);
   $('.main-controls .play-pause').html(playerBarPauseButton);
 };
 
